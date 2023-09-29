4cbcb4632e41c132803b39812ff305a0420bcacd
ixs-koki-chin <545705507@qq.com>
2023-09-29 09:42:40 +0900

    Update create_code_interpreter.py

--- interpreter/code_interpreters/create_code_interpreter.py
+++ interpreter/code_interpreters/create_code_interpreter.py
@@ -1,11 +1,11 @@
 from .language_map import language_map
 
-def create_code_interpreter(language):
+def create_code_interpreter(language,user=''):
     # Case in-sensitive
     language = language.lower()
 
     try:
         CodeInterpreter = language_map[language]
-        return CodeInterpreter()
+        return CodeInterpreter(user=user)
     except KeyError:
         raise ValueError(f"Unknown or unsupported language: {language}")
